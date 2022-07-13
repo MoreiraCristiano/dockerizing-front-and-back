6bdaa8bdc2546ec0e66565f77fe57d79fe26d3c7
Cristiano M <moreiracristiano702@gmail.com>
2022-07-13 00:47:57 +0000

    update

--- web_app/server.js
+++ web_app/server.js
@@ -6,12 +6,10 @@ const port = 8080;
 app.use(cors());
 
 app.get("/", (req, res) => {
-  res.sendFile(
-    "/home/cristiano/Docs/web_app_docker/web_app/frontend/index.html"
-  );
+  res.sendFile("/app/frontend/index.html");
 });
 
-app.use("/", express.static("../frontend"));
+app.use("/", express.static("./frontend"));
 
 app.listen(port, () => {
   console.log(`Running on port ${port}`);
